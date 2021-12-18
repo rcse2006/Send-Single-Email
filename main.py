06eec4eefd96c57cc3c432842803a22d93002ca4
rcse2006 <rcse2006@gmail.com>
2021-12-18 09:58:42 +0000

    Send Email Every Day

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import yagmail
 import os
 import time
+from datetime import datetime as dt
 
 sender = 'xyz@gmail.com'
 receiver = 'xyz@gmail.com'
@@ -13,7 +14,8 @@ Hi!
 """
 
 while True:
-  yag = yagmail.SMTP(user=sender, password=os.getenv('PASSWORD'))
-  yag.send(to=receiver, subject=subject, contents=contents)
-  print('Email Sent!')
-  time.sleep(60)
+  if dt.now().hour == 20 and dt.now().minute == 15:
+    yag = yagmail.SMTP(user=sender, password=os.getenv('PASSWORD'))
+    yag.send(to=receiver, subject=subject, contents=contents)
+    print('Email Sent!')
+    time.sleep(60)
